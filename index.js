fc7cd98cad8a874ea16b44e2900e04cbf9e2aa2e
FakeGDDreamSlenderman2.2 <gddreamslenderman@gmail.com>
2024-12-06 12:47:30 +0700

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching Nitro Video","Make The Nitro Gift Link","Playing Nitro"];
 
 
 let currentIndex = 0;
